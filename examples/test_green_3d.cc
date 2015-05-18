a2ee1c4b2137e620217f26de39368e10c10bb3ae
JiongCHEN <chenjiong1991@126.com>
2015-05-18 13:28:26 +0800

    test twisting a bar

--- examples/test_green_3d.cc
+++ examples/test_green_3d.cc
@@ -19,11 +19,22 @@ int main(int argc, char *argv[])
     def.load_cage(argv[2]);
     def.calc_green_coords();
 
-    const double disp[3] = {0.0, 1.0, 0.0};
-    def.move_cage(4, disp, true);
-    def.move_cage(5, disp, true);
-    def.move_cage(6, disp, true);
-    def.move_cage(7, disp, true);
+//    /// for sphere
+//    const double disp[3] = {0.0, -2.0, 0.0};
+//    def.move_cage(4, disp, true);
+//    def.move_cage(5, disp, true);
+//    def.move_cage(6, disp, true);
+//    def.move_cage(7, disp, true);
+
+    /// for bar
+    const double pos4[3] = {-1.031485, 4.966156, 1.048004};
+    const double pos5[3] = {-1.031485, 4.966156, -1.046975};
+    const double pos6[3] = {1.063854, 4.966156, -1.046975};
+    const double pos7[3] = {1.063854, 4.966156, 1.048004};
+    def.move_cage(4, pos5, false);
+    def.move_cage(5, pos6, false);
+    def.move_cage(6, pos7, false);
+    def.move_cage(7, pos4, false);
 
     def.deform();
 
