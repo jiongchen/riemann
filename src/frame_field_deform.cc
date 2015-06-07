82ea7d3aaf0de54e91d76f3a9a19efc242f93125
JiongChen <chenjiong1991@126.com>
2015-06-08 00:47:34 +0800

    design some interfaces for frame field deformer

--- src/frame_field_deform.cc
+++ src/frame_field_deform.cc
@@ -1,2 +1,11 @@
 #include "frame_field_deform.h"
 
+#include <iostream>
+#include <jtflib/mesh/io.h>
+
+using namespace std;
+using namespace Eigen;
+
+namespace geom_deform {
+
+}
--- src/frame_field_deform.h
+++ src/frame_field_deform.h
@@ -1,4 +1,36 @@
 #ifndef FRAME_FIELD_DEFORM_H
 #define FRAME_FIELD_DEFORM_H
 
+#include <Eigen/Sparse>
+
+namespace geom_deform {
+
+class frame_field_deform
+{
+public:
+  typedef Eigen::MatrixXd matd_t;
+  typedef Eigen::MatrixXi mati_t;
+  typedef Eigen::VectorXd vec_t;
+  typedef Eigen::SparseMatrix<double> spmat_t;
+  frame_field_deform();
+  // IO
+  int load_mesh(const char *path);
+  int load_constraints(const char *path);
+  int save_origin_mesh(const char *path) const;
+  int save_deformed_mesh(const char *path) const;
+
+  // prepare
+  int interp_frame_fields();
+
+  // deform
+  int deform();
+
+private:
+  mati_t tris_;
+  matd_t nods_;
+  std::vector<Eigen::Matrix2d> B_;
+  std::vector<Eigen::Vector3d> W_;
+};
+
+}
 #endif
