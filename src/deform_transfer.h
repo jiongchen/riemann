6fe27cf053c6e913c37a7848da9bbf3cbdd3731d
JiongCHEN <chenjiong1991@126.com>
2015-06-19 15:30:02 +0800

    get mesh data for deformation transfer

--- src/deform_transfer.h
+++ src/deform_transfer.h
@@ -16,15 +16,18 @@ public:
   int load_reference_source_mesh(const char *filename);
   int load_reference_target_mesh(const char *filename);
   int load_deformed_source_mesh(const char *filename);
-  int load_triangle_face_mapping(const char *filename);
+  int load_vertex_markers(const char *filename);
   int save_reference_source_mesh(const char *filename) const;
   int save_reference_target_mesh(const char *filename) const;
   int save_deformed_source_mesh(const char *filename) const;
   int save_deformed_target_mesh(const char *filename) const;
+  // correspondence solver
+  // deformation solver
 private:
   mati_t src_tris_, tar_tris_;
   matd_t src_ref_nods_, src_def_nods_;
   matd_t tar_ref_nods_, tar_def_nods_;
+  Eigen::MatrixXd Vinv_;
 };
 
 }
