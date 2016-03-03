5b8cc303737154832ceec898c16895c494cf53fd
JiongCHEN <gakki91@gmail.com>
2016-03-03 14:51:23 +0800

    use json to config hole filling

--- examples/test_hole_filling.cc
+++ examples/test_hole_filling.cc
@@ -5,6 +5,7 @@
 #include "igl/writeOBJ.h"
 #include "igl/boundary_loop.h"
 
+#include "src/json.h"
 #include "src/vtk.h"
 #include "src/hole_filling.h"
 #include "src/vtk.h"
@@ -17,37 +18,66 @@ using namespace riemann;
 int main(int argc, char *argv[])
 {
   if ( argc != 2 ) {
-    cerr << "# usage: prog model.obj\n";
+    cerr << "# usage: prog conf.json\n";
     return __LINE__;
   }
-  boost::filesystem::create_directory("./hole_filling");
+  Json::Reader reader;
+  Json::Value json;
+  ifstream ifs(argv[1]);
+  if ( ifs.fail() ) {
+    cerr << "[ERROR] can not open " << argv[1] << endl;
+    return __LINE__;
+  }
+  bool flag = reader.parse(ifs, json);
+  if ( !flag ) {
+    cerr << "[ERROR] " << reader.getFormattedErrorMessages() << endl;
+    return __LINE__;
+  }
 
-  mati tris; matd nods;
-  igl::readOBJ(argv[1], nods, tris);
-  igl::writeOBJ("./hole_filling/origin.obj", nods, tris);
+  boost::filesystem::create_directories(json["outdir"].asString());
+  char outfile[256];
+
+  eigen_mati_t tris; eigen_matd_t nods;
+  igl::readOBJ(json["mesh"].asString(), nods, tris);
+  sprintf(outfile, "%s/origin.obj", json["outdir"].asString().c_str());
+  igl::writeOBJ(outfile, nods, tris);
 
   boundary_loop loop;
+  select_method(loop, static_cast<boundary_loop::type>(json["method"].asInt()));
+
   get_boundary_loop(tris, nods, loop);
   calc_bnd_local_frame(tris, nods, loop);
-  calc_dl(loop);
-  cout << loop.dl << endl;
+  calc_infinitesimal_elem(loop);
+  calc_bnd_indicator(loop);
+  cout << loop.sf << endl;
 
-//  loop.T *= 0.1;
-//  loop.B *= 0.1;
-//  loop.N *= 0.1;
-//  draw_vert_direct_field("./hole_filling/T.vtk", loop.pos.data(), loop.pos.rows(), loop.T.data());
-//  draw_vert_direct_field("./hole_filling/B.vtk", loop.pos.data(), loop.pos.rows(), loop.B.data());
-//  draw_vert_direct_field("./hole_filling/N.vtk", loop.pos.data(), loop.pos.rows(), loop.N.data());
-
-  matd bbox(2, 3), pts;
-  bbox << -1, -1, -1, 1, 1, 1;
+  eigen_vecd_t center(3);
+  center << json["center"][0].asDouble(), json["center"][1].asDouble(), json["center"][2].asDouble();
+  double dx = json["dx"].asDouble();
+  eigen_matd_t bbox(2, 3), pts;
+  bbox << center(0)-dx, center(1)-dx, center(2)-dx,
+          center(0)+dx, center(1)+dx, center(2)+dx;
   cout << "[INFO] sampling bounding box:\n" << bbox << endl;
-  uniform_sampling(bbox, 10000, pts);
-  veci p = veci::Zero(pts.rows());
-  double count = 0;
-  std::generate(p.data(), p.data()+p.size(), [&count]()->double {return count++;});
-  ofstream os("./hole_filling/pc.vtk");
+  structured_grid_sampling(bbox, json["resolution"].asInt(), pts);
+
+  eigen_veci_t p = eigen_veci_t::Zero(pts.rows());
+  int count = 0;
+  std::generate(p.data(), p.data()+p.size(), [&count]()->int {return count++;});
+  sprintf(outfile, "%s/grid.m%d.vtk", json["outdir"].asString().c_str(), json["method"].asInt());
+  ofstream os(outfile);
   point2vtk(os, pts.data(), pts.rows(), p.data(), p.size());
+
+  eigen_vecd_t sf;
+  calc_scalar_field(loop, pts, sf);
+  point_data(os, sf.data(), sf.size(), "sf");
+
+  loop.T *= 0.1; loop.B *= 0.1; loop.N *= 0.1;
+  string outx = json["outdir"].asString()+string("/T.vtk");
+  draw_vert_direct_field(outx.c_str(), loop.pos.data(), loop.pos.rows(), loop.T.data());
+  string outy = json["outdir"].asString()+string("/B.vtk");
+  draw_vert_direct_field(outy.c_str(), loop.pos.data(), loop.pos.rows(), loop.B.data());
+  string outz = json["outdir"].asString()+string("/N.vtk");
+  draw_vert_direct_field(outz.c_str(), loop.pos.data(), loop.pos.rows(), loop.N.data());
 
   cout << "[INFO] done\n";
   return 0;
--- src/hole_filling.cc
+++ src/hole_filling.cc
@@ -2,25 +2,35 @@
 
 #include <random>
 #include <chrono>
+#include <Eigen/UmfPackSupport>
 
 #include "igl/boundary_loop.h"
 #include "igl/per_vertex_normals.h"
 #include "igl/cross.h"
+#include "config.h"
 
 using namespace std;
 using namespace Eigen;
 
 namespace riemann {
 
-void get_boundary_loop(const mati &tris, const matd &nods, boundary_loop &loop) {
+void get_boundary_loop(const eigen_mati_t &tris, const eigen_matd_t &nods, boundary_loop &loop) {
   igl::boundary_loop(tris, loop.bnd);
   loop.pos.resize(loop.bnd.size(), 3);
   for (int i = 0; i < loop.pos.rows(); ++i)
     loop.pos.row(i) = nods.row(loop.bnd(i));
 }
 
-void calc_bnd_local_frame(const mati &tris, const matd &nods, boundary_loop &loop) {
-  matd vn;
+void select_method(boundary_loop &loop, boundary_loop::type t) {
+  loop.method = static_cast<int>(t);
+  if ( loop.method != 0 && loop.method != 1 ) {
+    cerr << "[ERROR] method not supported!\n";
+    exit(EXIT_FAILURE);
+  }
+}
+
+void calc_bnd_local_frame(const eigen_mati_t &tris, const eigen_matd_t &nods, boundary_loop &loop) {
+  eigen_matd_t vn;
   igl::per_vertex_normals(nods, tris, igl::PER_VERTEX_NORMALS_WEIGHTING_TYPE_AREA, vn);
   const int bnd_size = loop.bnd.size();
   loop.T.resize(bnd_size, 3);
@@ -34,37 +44,72 @@ void calc_bnd_local_frame(const mati &tris, const matd &nods, boundary_loop &loo
   }
 }
 
-void calc_dl(boundary_loop &loop) {
+void calc_infinitesimal_elem(boundary_loop &loop) {
   const int bnd_size = loop.bnd.size();
   loop.dl.setZero(bnd_size);
   for (int i = 0; i < bnd_size; ++i) {
-    loop.dl(i) += (loop.pos.row(i)-loop.pos.row((i-1)%bnd_size)).norm();
+    loop.dl(i) += (loop.pos.row(i)-loop.pos.row((i-1+bnd_size)%bnd_size)).norm();
     loop.dl(i) += (loop.pos.row(i)-loop.pos.row((i+1)%bnd_size)).norm();
     loop.dl(i) /= 2.0;
   }
 }
 
-//void init_bnd_scalar_field(boundary_loop &loop) {
-//  const int bnd_size = loop.bnd.size();
-//  loop.sf.setZero(bnd_size);
-//  for (int i = 0; i < bnd_size; ++i) {
-//    double integral = 0.0;
-//    for (int k = 0; k < bnd_size; ++k) {
-//      integral += (loop.pos.row(i)-loop.pos.row(k)).dot(loop.B.row(k))*loop.dl(k);
-//    }
-//    loop.sf(i) = integral/loop.dl.sum();
-//  }
-//}
+void calc_bnd_indicator(boundary_loop &loop) {
+  const int bnd_size = loop.bnd.size();
+  loop.sf.setZero(bnd_size);
+  if ( loop.method == 0 ) {
+    for (int i = 0; i < bnd_size; ++i) {
+      double integral = 0.0;
+      for (int k = 0; k < bnd_size; ++k) {
+        integral += (loop.pos.row(i)-loop.pos.row(k)).dot(loop.B.row(k))*loop.dl(k);
+      }
+      loop.sf(i) = -integral;
+    }
+  } else {
+    MatrixXd A(bnd_size, bnd_size); VectorXd b(bnd_size);
+    b.setZero();
+    for (int i = 0; i < bnd_size; ++i) {
+      for (int j = 0; j < bnd_size; ++j) {
+        A(i, j) = (loop.pos.row(i)-loop.pos.row(j)).dot(loop.B.row(j))*loop.dl(j);
+      }
+    }
+    cout << A.rows() << endl;
+    FullPivLU<MatrixXd> solver;
+    solver.compute(A);
+    cout << solver.rank() << endl;
+    getchar();
+    VectorXd x = solver.solve(b);
+    std::copy(x.data(), x.data()+x.size(), loop.sf.data());
+  }
+}
 
-//double indicator_value(const vecd &x, const boundary_loop &loop) {
-//  double rtn = 0.0;
-//  for (int i = 0; i < loop.bnd.size(); ++i) {
-//    rtn += ((x-loop.pos.row(i)).dot(loop.B.row(i))-loop.sf(i))*loop.dl(i);
-//  }
-//  return rtn;
-//}
+static inline double calc_rbf(const eigen_vecd_t &x, const eigen_vecd_t &c) {
+  return 1.0/(x-c).norm();
+}
 
-void uniform_sampling(const matd &box, const int num, matd &pts) {
+double indicator_value(const eigen_vecd_t &x, const boundary_loop &loop) {
+  double rtn = 0.0;
+  if ( loop.method == 0 ) {
+    double integral = 0.0, correction = 0.0, sum = 0.0;
+    // first part
+    for (int i = 0; i < loop.bnd.size(); ++i) {
+      integral += ((x-loop.pos.row(i)).dot(loop.B.row(i)))*loop.dl(i);
+    }
+    // second part
+    for (int i = 0; i < loop.bnd.size(); ++i) {
+      double rbf = calc_rbf(x, loop.pos.row(i));
+      correction += rbf*loop.sf(i)*loop.dl(i);
+      sum += rbf*loop.dl(i);
+    }
+    rtn = integral+correction/sum;
+  } else {
+    for (int i = 0; i < loop.bnd.size(); ++i)
+      rtn += ((x-loop.pos.row(i)).dot(loop.sf(i)*loop.B.row(i)))*loop.dl(i);
+  }
+  return rtn;
+}
+
+void uniform_random_sampling(const eigen_matd_t &box, const int num, eigen_matd_t &pts) {
   double xmin = box(0, 0), xmax = box(1, 0);
   double ymin = box(0, 1), ymax = box(1, 1);
   double zmin = box(0, 2), zmax = box(1, 2);
@@ -76,6 +121,33 @@ void uniform_sampling(const matd &box, const int num, matd &pts) {
     pts(i, 0) = xmin+distribution(generator)*(xmax-xmin);
     pts(i, 1) = ymin+distribution(generator)*(ymax-ymin);
     pts(i, 2) = zmin+distribution(generator)*(zmax-zmin);
+  }
+}
+
+void structured_grid_sampling(const eigen_matd_t &box, const int res, eigen_matd_t &pts) {
+  double xmin = box(0, 0), xmax = box(1, 0);
+  double ymin = box(0, 1), ymax = box(1, 1);
+  double zmin = box(0, 2), zmax = box(1, 2);
+  vector<double> buffer;
+  double dx = (xmax-xmin)/res, dy = (ymax-ymin)/res, dz = (zmax-zmin)/res;
+  for (int i = 0; i <= res; ++i) {
+    for (int j = 0; j <= res; ++j) {
+      for (int k = 0; k <= res; ++k) {
+        buffer.push_back(xmin+i*dx);
+        buffer.push_back(ymin+j*dy);
+        buffer.push_back(zmin+k*dz);
+      }
+    }
+  }
+  pts.resize(buffer.size()/3, 3);
+  std::copy(buffer.begin(), buffer.end(), pts.data());
+}
+
+void calc_scalar_field(const boundary_loop &loop, const eigen_matd_t &pts, eigen_vecd_t &sf) {
+  sf.setZero(pts.rows());
+#pragma omp parallel for
+  for (int i = 0; i < sf.size(); ++i) {
+    sf(i) = indicator_value(pts.row(i), loop);
   }
 }
 
