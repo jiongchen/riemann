fbb228962e47241c0552caff6c21160b370f54cf
JiongCHEN <gakki91@gmail.com>
2016-07-04 16:05:38 +0800

    add maxima script for SH related derivation

---
+++ examples/test_vol_frame.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <jtflib/mesh/mesh.h>
+
+#include "src/volume_frame.h"
+
+using namespace std;
+using jtf::mesh::face2tet_adjacent;
+
+int main(int argc, char *argv[])
+{
+
+  // shared_ptr<face2tet_adjacent> f2t;
+  // jtf::mesh::get_outside_face();
+  
+  cout << "[Info] done\n";
+  return 0;
+}
--- src/volume_frame.h
+++ src/volume_frame.h
@@ -1,7 +1,67 @@
 #ifndef VOLUME_FRAME_H
 #define VOLUME_FRAME_H
 
+#include <zjucad/matrix/matrix.h>
+
+#include "def.h"
+
 namespace riemann {
+
+static const double g_RXYZ [24][3][3]={
+  {{1,0,0},{0,0,1},{0,-1,0}},  //  u1
+  {{1,0,0},{0,-1,0},{0,0,-1}}, //  u2
+  {{1,0,0},{0,0,-1},{0,1,0}},  //  u3
+  {{0,0,-1},{0,1,0},{1,0,0}},  //  v1
+  {{-1,0,0},{0,1,0},{0,0,-1}}, //  v2
+  {{0,0,1},{0,1,0},{-1,0,0}},  //  v3
+  {{0,1,0},{-1,0,0},{0,0,1}},  //  w1
+  {{-1,0,0},{0,-1,0},{0,0,1}}, //  w2
+  {{0,-1,0},{1,0,0},{0,0,1}},  //  w3
+  {{1,0,0},{0,1,0},{0,0,1}},   //  I
+  {{-1,0,0},{0,0,1},{0,1,0}},   //  u1 * v2, u3 * w2, w2 * u1
+  {{-1,0,0},{0,0,-1},{0,-1,0}}, //  v2 * u1, u3 * v2
+  {{0,0,1},{1,0,0},{0,1,0}},    //  v3 * u3, w3 * v3,  u3*w3
+  {{0,0,-1},{1,0,0},{0,-1,0}},  //  v1 * u1, w3 * v1,
+  {{0,0,-1},{-1,0,0},{0,1,0}},  //  v1 * u3, u3 * w1,
+  {{0,0,1},{-1,0,0},{0,-1,0}},  //  v3 * u1, u1 * w1
+  {{0,1,0},{0,0,1},{1,0,0}},    //  u1 * v1, v1 * w1, w1 * u1
+  {{0,-1,0},{0,0,-1},{1,0,0}},  //  u3 * v1, w3 * u3
+  {{0,0,1},{0,-1,0},{1,0,0}},   //  u2 * v1, v3 * u2
+  {{0,1,0},{0,0,-1},{-1,0,0}},  //  u3 * v3, v3 * w1
+  {{0,-1,0},{0,0,1},{-1,0,0}},  //  u1 * v3, w3 * u1, v3 * w3,
+  {{0,0,-1},{0,-1,0},{-1,0,0}}, //  v1 * u2, w2 * v1, v3 * w2
+  {{0,1,0},{1,0,0},{0,0,-1}},   //  u2 * w3, v2 * w1, w3 * v2
+  {{0,-1,0},{-1,0,0},{0,0,-1}}  //  u2 * w1, w3 * u2
+};
+
+using mati_t=zjucad::matrix::matrix<size_t>;
+using matd_t=zjucad::matrix::matrix<double>;
+
+class SH_smooth_energy : public Functional<double>
+{
+public:
+  SH_smooth_energy(const mati_t &tets, const matd_t &nods, const double w);
+  size_t Nx() const;
+  int Val(const double *abc, double *val) const;
+  int Gra(const double *abc, double *gra) const;
+  int Hes(const double *abc, std::vector<Eigen::Triplet<double>> *hes) const { return __LINE__; }
+private:
+  const mati_t &tets_;
+  const matd_t &nods_;
+};
+
+class SH_align_energy : public Functional<double>
+{
+public:
+  SH_align_energy(const mati_t &tris, const matd_t &nods, const double w);
+  size_t Nx() const;
+  int Val(const double *abc, double *val) const;
+  int Gra(const double *abc, double *gra) const;
+  int Hes(const double *abc, std::vector<Eigen::Triplet<double>> *hes) const { return __LINE__; }
+private:
+  const mati_t &tets_;
+  const matd_t &nods_;
+};
 
 }
 
