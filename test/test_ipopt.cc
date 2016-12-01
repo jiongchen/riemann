2c2d5557e0be31e326bed80256847a27b4a525aa
JiongCHEN <gakki91@gmail.com>
2016-12-01 23:00:37 +0800

    reinstall Ipopt, update examples

--- test/test_ipopt.cc
+++ test/test_ipopt.cc
@@ -1,55 +1,64 @@
 #include <iostream>
-
 #include <gtest/gtest.h>
 #include <IpIpoptApplication.hpp>
 #include <IpSolveStatistics.hpp>
 #include <IpTNLP.hpp>
+#include <memory>
 
 using namespace Ipopt;
+using namespace std;
 
 /** C++ Example NLP for interfacing a problem with IPOPT.
- *  MyNLP implements a C++ example showing how to interface with IPOPT
- *  through the TNLP interface. This example is designed to go along with
- *  the tutorial document (see Examples/CppTutorial/).
- *  This class implements the following NLP.
+ *  HS071_NLP implements a C++ example of problem 71 of the
+ *  Hock-Schittkowski test suite. This example is designed to go
+ *  along with the tutorial document and show how to interface
+ *  with IPOPT through the TNLP interface. 
  *
- * min_x f(x) = -(x2-2)^2
- *  s.t.
- *       0 = x1^2 + x2 - 1
- *       -1 <= x1 <= 1
+ * Problem hs071 looks like this
+ *
+ *     min   x1*x4*(x1 + x2 + x3)  +  x3
+ *     s.t.  x1*x2*x3*x4                   >=  25
+ *           x1**2 + x2**2 + x3**2 + x4**2  =  40
+ *           1 <=  x1,x2,x3,x4  <= 5
+ *
+ *     Starting point:
+ *        x = (1, 5, 5, 1)
+ *
+ *     Optimal solution:
+ *        x = (1.00000000, 4.74299963, 3.82114998, 1.37940829)
+ *
  *
  */
-class MyNLP : public TNLP
+class HS071_NLP : public TNLP
 {
 public:
   /** default constructor */
-  MyNLP();
+  HS071_NLP();
 
   /** default destructor */
-  virtual ~MyNLP();
+  virtual ~HS071_NLP();
 
   /**@name Overloaded from TNLP */
   //@{
   /** Method to return some info about the nlp */
   virtual bool get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
-                              Index& nnz_h_lag, IndexStyleEnum& index_style);
-
+                            Index& nnz_h_lag, IndexStyleEnum& index_style);
 
   /** Method to return the bounds for my problem */
   virtual bool get_bounds_info(Index n, Number* x_l, Number* x_u,
-                                 Index m, Number* g_l, Number* g_u);
+                               Index m, Number* g_l, Number* g_u);
 
   /** Method to return the starting point for the algorithm */
   virtual bool get_starting_point(Index n, bool init_x, Number* x,
                                   bool init_z, Number* z_L, Number* z_U,
                                   Index m, bool init_lambda,
-                                    Number* lambda);
+                                  Number* lambda);
 
   /** Method to return the objective value */
-virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value);
+  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value);
 
   /** Method to return the gradient of the objective */
-virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f);
+  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f);
 
   /** Method to return the constraint residuals */
   virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g);
@@ -85,233 +94,305 @@ virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f);
   //@}
 
 private:
-  /**@name Methods to block default compiler methods.
-   * The compiler automatically generates the following three methods.
-   *  Since the default compiler implementation is generally not what
-   *  you want (for all but the most simple classes), we usually 
-   *  put the declarations of these methods in the private section
-   *  and never implement them. This prevents the compiler from
-   *  implementing an incorrect "default" behavior without us
-   *  knowing. (See Scott Meyers book, "Effective C++")
-   *  
-   */
-  //@{
-  //  MyNLP();
-  MyNLP(const MyNLP&);
-  MyNLP& operator=(const MyNLP&);
-  //@}
+  HS071_NLP(const HS071_NLP&);
+  HS071_NLP& operator=(const HS071_NLP&);
 };
 
-MyNLP::MyNLP()
+// constructor
+HS071_NLP::HS071_NLP()
 {}
 
-MyNLP::~MyNLP()
+//destructor
+HS071_NLP::~HS071_NLP()
 {}
 
-bool MyNLP::get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
-                         Index& nnz_h_lag, IndexStyleEnum& index_style)
+// returns the size of the problem
+bool HS071_NLP::get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
+                             Index& nnz_h_lag, IndexStyleEnum& index_style)
 {
-  // The problem described in MyNLP.hpp has 2 variables, x1, & x2,
-  n = 2;
+  // The problem described in HS071_NLP.hpp has 4 variables, x[0] through x[3]
+  n = 4;
 
-  // one equality constraint,
-  m = 1;
+  // one equality constraint and one inequality constraint
+  m = 2;
 
-  // 2 nonzeros in the jacobian (one for x1, and one for x2),
-  nnz_jac_g = 2;
+  // in this example the jacobian is dense and contains 8 nonzeros
+  nnz_jac_g = 8;
 
-  // and 2 nonzeros in the hessian of the lagrangian
-  // (one in the hessian of the objective for x2,
-  //  and one in the hessian of the constraints for x1)
-  nnz_h_lag = 2;
+  // the hessian is also dense and has 16 total nonzeros, but we
+  // only need the lower left corner (since it is symmetric)
+  nnz_h_lag = 10;
 
-  // We use the standard fortran index style for row/col entries
-  index_style = FORTRAN_STYLE;
+  // use the C style indexing (0-based)
+  index_style = TNLP::C_STYLE;
 
   return true;
 }
 
-bool MyNLP::get_bounds_info(Index n, Number* x_l, Number* x_u,
-                            Index m, Number* g_l, Number* g_u)
+// returns the variable bounds
+bool HS071_NLP::get_bounds_info(Index n, Number* x_l, Number* x_u,
+                                Index m, Number* g_l, Number* g_u)
 {
   // here, the n and m we gave IPOPT in get_nlp_info are passed back to us.
   // If desired, we could assert to make sure they are what we think they are.
-  assert(n == 2);
-  assert(m == 1);
+  assert(n == 4);
+  assert(m == 2);
 
-  // x1 has a lower bound of -1 and an upper bound of 1
-  x_l[0] = -1.0;
-  x_u[0] = 1.0;
+  // the variables have lower bounds of 1
+  for (Index i=0; i<4; i++) {
+    x_l[i] = 1.0;
+  }
 
-  // x2 has no upper or lower bound, so we set them to
-  // a large negative and a large positive number.
-  // The value that is interpretted as -/+infinity can be
-  // set in the options, but it defaults to -/+1e19
-  x_l[1] = -1.0e19;
-  x_u[1] = +1.0e19;
+  // the variables have upper bounds of 5
+  for (Index i=0; i<4; i++) {
+    x_u[i] = 5.0;
+  }
 
-  // we have one equality constraint, so we set the bounds on this constraint
-  // to be equal (and zero).
-  g_l[0] = g_u[0] = 0.0;
+  // the first constraint g1 has a lower bound of 25
+  g_l[0] = 25;
+  // the first constraint g1 has NO upper bound, here we set it to 2e19.
+  // Ipopt interprets any number greater than nlp_upper_bound_inf as
+  // infinity. The default value of nlp_upper_bound_inf and nlp_lower_bound_inf
+  // is 1e19 and can be changed through ipopt options.
+  g_u[0] = 2e19;
+
+  // the second constraint g2 is an equality constraint, so we set the
+  // upper and lower bound to the same value
+  g_l[1] = g_u[1] = 40.0;
 
   return true;
 }
 
-bool MyNLP::get_starting_point(Index n, bool init_x, Number* x,
-                               bool init_z, Number* z_L, Number* z_U,
-                               Index m, bool init_lambda,
-                               Number* lambda)
+// returns the initial point for the problem
+bool HS071_NLP::get_starting_point(Index n, bool init_x, Number* x,
+                                   bool init_z, Number* z_L, Number* z_U,
+                                   Index m, bool init_lambda,
+                                   Number* lambda)
 {
   // Here, we assume we only have starting values for x, if you code
-  // your own NLP, you can provide starting values for the others if
-  // you wish.
+  // your own NLP, you can provide starting values for the dual variables
+  // if you wish
   assert(init_x == true);
   assert(init_z == false);
   assert(init_lambda == false);
 
-  // we initialize x in bounds, in the upper right quadrant
-  x[0] = 0.5;
-  x[1] = 1.5;
+  // initialize to the given starting point
+  x[0] = 1.0;
+  x[1] = 5.0;
+  x[2] = 5.0;
+  x[3] = 1.0;
 
   return true;
 }
 
-bool MyNLP::eval_f(Index n, const Number* x, bool new_x, Number& obj_value)
+// returns the value of the objective function
+bool HS071_NLP::eval_f(Index n, const Number* x, bool new_x, Number& obj_value)
 {
-  // return the value of the objective function
-  Number x2 = x[1];
-  obj_value = -(x2 - 2.0) * (x2 - 2.0);
+  assert(n == 4);
+
+  obj_value = x[0] * x[3] * (x[0] + x[1] + x[2]) + x[2];
+
   return true;
 }
 
-bool MyNLP::eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f)
+// return the gradient of the objective function grad_{x} f(x)
+bool HS071_NLP::eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f)
 {
-  // return the gradient of the objective function grad_{x} f(x)
+  assert(n == 4);
 
-  // grad_{x1} f(x): x1 is not in the objective
-  grad_f[0] = 0.0;
-
-  // grad_{x2} f(x):
-  Number x2 = x[1];
-  grad_f[1] = -2.0*(x2 - 2.0);
+  grad_f[0] = x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]);
+  grad_f[1] = x[0] * x[3];
+  grad_f[2] = x[0] * x[3] + 1;
+  grad_f[3] = x[0] * (x[0] + x[1] + x[2]);
 
   return true;
 }
 
-bool MyNLP::eval_g(Index n, const Number* x, bool new_x, Index m, Number* g)
+// return the value of the constraints: g(x)
+bool HS071_NLP::eval_g(Index n, const Number* x, bool new_x, Index m, Number* g)
 {
-  // return the value of the constraints: g(x)
-  Number x1 = x[0];
-  Number x2 = x[1];
+  assert(n == 4);
+  assert(m == 2);
 
-  g[0] = -(x1*x1 + x2 - 1.0);
+  g[0] = x[0] * x[1] * x[2] * x[3];
+  g[1] = x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
 
   return true;
 }
 
-bool MyNLP::eval_jac_g(Index n, const Number* x, bool new_x,
-                       Index m, Index nele_jac, Index* iRow, Index *jCol,
-                       Number* values)
+// return the structure or values of the jacobian
+bool HS071_NLP::eval_jac_g(Index n, const Number* x, bool new_x,
+                           Index m, Index nele_jac, Index* iRow, Index *jCol,
+                           Number* values)
 {
   if (values == NULL) {
-    // return the structure of the jacobian of the constraints
+    // return the structure of the jacobian
 
-    // element at 1,1: grad_{x1} g_{1}(x)
-    iRow[0] = 1;
-    jCol[0] = 1;
-
-    // element at 1,2: grad_{x2} g_{1}(x)
-    iRow[1] = 1;
-    jCol[1] = 2;
+    // this particular jacobian is dense
+    iRow[0] = 0;
+    jCol[0] = 0;
+    iRow[1] = 0;
+    jCol[1] = 1;
+    iRow[2] = 0;
+    jCol[2] = 2;
+    iRow[3] = 0;
+    jCol[3] = 3;
+    iRow[4] = 1;
+    jCol[4] = 0;
+    iRow[5] = 1;
+    jCol[5] = 1;
+    iRow[6] = 1;
+    jCol[6] = 2;
+    iRow[7] = 1;
+    jCol[7] = 3;
   }
   else {
     // return the values of the jacobian of the constraints
-    Number x1 = x[0];
 
-    // element at 1,1: grad_{x1} g_{1}(x)
-    values[0] = -2.0 * x1;
+    values[0] = x[1]*x[2]*x[3]; // 0,0
+    values[1] = x[0]*x[2]*x[3]; // 0,1
+    values[2] = x[0]*x[1]*x[3]; // 0,2
+    values[3] = x[0]*x[1]*x[2]; // 0,3
 
-    // element at 1,2: grad_{x1} g_{1}(x)
-    values[1] = -1.0;
+    values[4] = 2*x[0]; // 1,0
+    values[5] = 2*x[1]; // 1,1
+    values[6] = 2*x[2]; // 1,2
+    values[7] = 2*x[3]; // 1,3
   }
 
   return true;
 }
 
-bool MyNLP::eval_h(Index n, const Number* x, bool new_x,
-                   Number obj_factor, Index m, const Number* lambda,
-                   bool new_lambda, Index nele_hess, Index* iRow,
-                   Index* jCol, Number* values)
+//return the structure or values of the hessian
+bool HS071_NLP::eval_h(Index n, const Number* x, bool new_x,
+                       Number obj_factor, Index m, const Number* lambda,
+                       bool new_lambda, Index nele_hess, Index* iRow,
+                       Index* jCol, Number* values)
 {
   if (values == NULL) {
     // return the structure. This is a symmetric matrix, fill the lower left
     // triangle only.
 
-    // element at 1,1: grad^2_{x1,x1} L(x,lambda)
-    iRow[0] = 1;
-    jCol[0] = 1;
+    // the hessian for this problem is actually dense
+    Index idx=0;
+    for (Index row = 0; row < 4; row++) {
+      for (Index col = 0; col <= row; col++) {
+        iRow[idx] = row;
+        jCol[idx] = col;
+        idx++;
+      }
+    }
 
-    // element at 2,2: grad^2_{x2,x2} L(x,lambda)
-    iRow[1] = 2;
-    jCol[1] = 2;
-
-    // Note: off-diagonal elements are zero for this problem
+    assert(idx == nele_hess);
   }
   else {
-    // return the values
+    // return the values. This is a symmetric matrix, fill the lower left
+    // triangle only
 
-    // element at 1,1: grad^2_{x1,x1} L(x,lambda)
-    values[0] = -2.0 * lambda[0];
+    // fill the objective portion
+    values[0] = obj_factor * (2*x[3]); // 0,0
 
-    // element at 2,2: grad^2_{x2,x2} L(x,lambda)
-    values[1] = -2.0 * obj_factor;
+    values[1] = obj_factor * (x[3]);   // 1,0
+    values[2] = 0.;                    // 1,1
 
-    // Note: off-diagonal elements are zero for this problem
+    values[3] = obj_factor * (x[3]);   // 2,0
+    values[4] = 0.;                    // 2,1
+    values[5] = 0.;                    // 2,2
+
+    values[6] = obj_factor * (2*x[0] + x[1] + x[2]); // 3,0
+    values[7] = obj_factor * (x[0]);                 // 3,1
+    values[8] = obj_factor * (x[0]);                 // 3,2
+    values[9] = 0.;                                  // 3,3
+
+
+    // add the portion for the first constraint
+    values[1] += lambda[0] * (x[2] * x[3]); // 1,0
+
+    values[3] += lambda[0] * (x[1] * x[3]); // 2,0
+    values[4] += lambda[0] * (x[0] * x[3]); // 2,1
+
+    values[6] += lambda[0] * (x[1] * x[2]); // 3,0
+    values[7] += lambda[0] * (x[0] * x[2]); // 3,1
+    values[8] += lambda[0] * (x[0] * x[1]); // 3,2
+
+    // add the portion for the second constraint
+    values[0] += lambda[1] * 2; // 0,0
+
+    values[2] += lambda[1] * 2; // 1,1
+
+    values[5] += lambda[1] * 2; // 2,2
+
+    values[9] += lambda[1] * 2; // 3,3
   }
 
   return true;
 }
 
-void MyNLP::finalize_solution(SolverReturn status,
-                              Index n, const Number* x, const Number* z_L, const Number* z_U,
-                              Index m, const Number* g, const Number* lambda,
-                              Number obj_value,
-			      const IpoptData* ip_data,
-			      IpoptCalculatedQuantities* ip_cq)
+void HS071_NLP::finalize_solution(SolverReturn status,
+                                  Index n, const Number* x, const Number* z_L, const Number* z_U,
+                                  Index m, const Number* g, const Number* lambda,
+                                  Number obj_value,
+				  const IpoptData* ip_data,
+				  IpoptCalculatedQuantities* ip_cq)
 {
   // here is where we would store the solution to variables, or write to a file, etc
-  // so we could use the solution. Since the solution is displayed to the console,
-  // we currently do nothing here.
+  // so we could use the solution.
+
+  // For this example, we write the solution to the console
+  std::cout << std::endl << std::endl << "Solution of the primal variables, x" << std::endl;
+  for (Index i=0; i<n; i++) {
+     std::cout << "x[" << i << "] = " << x[i] << std::endl;
+  }
+
+  std::cout << std::endl << std::endl << "Solution of the bound multipliers, z_L and z_U" << std::endl;
+  for (Index i=0; i<n; i++) {
+    std::cout << "z_L[" << i << "] = " << z_L[i] << std::endl;
+  }
+  for (Index i=0; i<n; i++) {
+    std::cout << "z_U[" << i << "] = " << z_U[i] << std::endl;
+  }
+
+  std::cout << std::endl << std::endl << "Objective value" << std::endl;
+  std::cout << "f(x*) = " << obj_value << std::endl;
+
+  std::cout << std::endl << "Final value of the constraints:" << std::endl;
+  for (Index i=0; i<m ;i++) {
+    std::cout << "g(" << i << ") = " << g[i] << std::endl;
+  }
 }
 
 TEST(ipopt, ipopt)
 {
-  // Create an instance of your nlp...
-  SmartPtr<TNLP> mynlp = new MyNLP();
+  SmartPtr<TNLP> mynlp = new HS071_NLP();
 
-  // Create an instance of the IpoptApplication
-  //
-  // We are using the factory, since this allows us to compile this
-  // example with an Ipopt Windows DLL
   SmartPtr<IpoptApplication> app = IpoptApplicationFactory();
+  app->RethrowNonIpoptException(true);
+
+  app->Options()->SetNumericValue("tol", 1e-7);
+  app->Options()->SetStringValue("mu_strategy", "adaptive");
+  app->Options()->SetStringValue("output_file", "ipopt.out");
+  
+  // The following overwrites the default name (ipopt.opt) of the
+  // options file
+  // app->Options()->SetStringValue("option_file_name", "hs071.opt");
 
   // Initialize the IpoptApplication and process the options
   ApplicationReturnStatus status;
   status = app->Initialize();
+  
   if (status != Solve_Succeeded) {
     std::cout << std::endl << std::endl << "*** Error during initialization!" << std::endl;
-    exit(0);
+    exit(1);
   }
-
+  
+  // Ask Ipopt to solve the problem
   status = app->OptimizeTNLP(mynlp);
 
   if (status == Solve_Succeeded) {
-    // Retrieve some statistics about the solve
-    Index iter_count = app->Statistics()->IterationCount();
-    std::cout << std::endl << std::endl << "*** The problem solved in " << iter_count << " iterations!" << std::endl;
-
-    Number final_obj = app->Statistics()->FinalObjective();
-    std::cout << std::endl << std::endl << "*** The final value of the objective function is " << final_obj << '.' << std::endl;
+    std::cout << std::endl << std::endl << "*** The problem solved!" << std::endl;
+  }
+  else {
+    std::cout << std::endl << std::endl << "*** The problem FAILED!" << std::endl;
   }
 
   EXPECT_EQ((int) status, 0);
