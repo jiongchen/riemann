1be3fe1a4b0cb1ae81c4dab376b6d45736b3d62e
jchen <jiong.chen@inria.fr>
2025-05-18 21:32:32 +0200

    example: vector deformer

--- examples/test_vec_field_deform.cc
+++ examples/test_vec_field_deform.cc
@@ -8,35 +8,24 @@ using namespace Eigen;
 
 int main(int argc, char *argv[])
 {
-    if ( argc != 2 ) {
-        cerr << "# Usage: " << argv[0] << " model.obj\n";
-        return __LINE__;
-    }
-    boost::filesystem::create_directory("./vel_field_deform");
+  boost::filesystem::create_directories("./result/vel_field_deform");
 
-    riemann::vel_field_deform def;
-    def.load_model(argv[1]);
+  riemann::vel_field_deform def;
+  def.load_model("./dat/beam.obj");
 
-//    // for sphere
-//    Vector3d src(0, 1.0, 0);
-//    Vector3d des(0, 1.2, 0);
-//    const double ri = 0.01;
-//    const double ro = 0.8;
-//    def.translate_deform(src, des, ri, ro);
+  // // for beam twist
+  // Vector3d center(-6, 0, 0), n(1, 0, 0);
+  // const double ri = 12.05;
+  // const double ro = 14;
+  // def.twist_deform(center, ri, ro, n, 400);
 
-//    // for beam twist
-//    Vector3d center(-6, 0, 0), n(1, 0, 0);
-//    const double ri = 12.05;
-//    const double ro = 14;
-//    def.twist_deform(center, ri, ro, n, 400);
+  // for beam bend
+  Vector3d center(0, 0, 0), n(1, 0, 0), axis(0, 0, -1);
+  const double ri = -2;
+  const double ro = 2;
+  def.bend_deform(center, ri, ro, axis, n, M_PI/2);
 
-    // for beam bend
-    Vector3d center(0, 0, 0), n(1, 0, 0), axis(0, 0, -1);
-    const double ri = -2;
-    const double ro = 2;
-    def.bend_deform(center, ri, ro, axis, n, M_PI/2);
-
-    def.save_model("./vel_field_deform/deform.obj");
-    cout << "done\n";
-    return 0;
+  def.save_model("./result/vel_field_deform/deform.obj");
+  cout << "done\n";
+  return 0;
 }
---
+++ run_vec_deformer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+build/examples/test_vec_field_deform
+cp dat/beam.obj result/vel_field_deform/rest.obj
